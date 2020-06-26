d9c9605758b5c0b8891be1444615479a006023ca
nishitha04 <anugulanishithareddy04@gmail.com>
2020-06-26 14:19:14 +1200

    Mapview
    Mapview

---
+++ app/src/main/java/com/example/chargeme/Mapview.java
@@ -0,0 +1,14 @@
+package com.example.chargeme;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Mapview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mapview);
+    }
+}
